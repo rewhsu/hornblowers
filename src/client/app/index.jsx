a189ed2bba81f46a0714d280277a8c68053adf21
Jennifer Sha <jennifersha@Jennifers-MacBook-Pro.local>
2017-02-01 19:52:08 -0800

    (refactor) Delete extra login/signup templates, fix index

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Login from './login.jsx';
-import Signup from './signup.jsx';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
@@ -10,18 +8,9 @@ class App extends React.Component {
     return (
       <div>
         <p> Hello Reacttt!</p>
-        <Login/>
-        <Signup/>
       </div>
     )
   }
 }
 
-render(<Router history={ createBrowserHistory() }>
-      <Route path="/" component={App}>
-      <IndexRoute component={App}/>
-
-        <Route path="/login" component={ Login } />
-       </Route>
-
-    </Router>, document.getElementById('app'));
+render(<App/>, document.getElementById('app'));
