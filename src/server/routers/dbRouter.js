6e8d92e6268f2aceb156cad547511726665e356f
Bianca Woo <woobianca@gmail.com>
2017-02-04 11:43:31 -0800

    (dev) Edit and add db routes

--- src/server/routers/dbRouter.js
+++ src/server/routers/dbRouter.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require('../db/index.js');
 
 var dbRouter = express.Router();
 module.exports = dbRouter;
@@ -7,8 +8,8 @@ module.exports = dbRouter;
 dbRouter.use(bodyParser.urlencoded({ extended: true }))
 dbRouter.use(bodyParser.json())
 
-
-dbRouter.get('/api/db/friends', function(req, res) {
+// FIND ALL USER'S FRIENDS BY USERID
+dbRouter.get('/friends', function(req, res) {
   db.User.findAll({
     where: {id: req.query.userid},
     include: [
@@ -16,31 +17,81 @@ dbRouter.get('/api/db/friends', function(req, res) {
     ]
   }).then(function(users) {
     if (users[0].Friends.length > 0) {
-      res.send(users[0].Friends)
+      res.json(users[0].Friends)
     } else {
       res.send('User does not have friends.')
     }
   })
 })
 
-dbRouter.get('/api/db/users', function(req, res) {
-  console.log('REQ DATA: ', req)
+// FIND USER BY SEARCHED NAME
+dbRouter.get('/users', function(req, res) {
   db.User.findOne({
     where: {
       user_name: req.query.searchName
     }
   }).then(function(user) {
-    console.log('USER searchName: ', user);
     if (user) {
-      res.send(user)
+      res.json(user)
     } else {
       res.send('Sorry, user not found. Please tell them to signup!')
     }
   })
 });
 
-dbRouter.post('/api/db/signup', function(req, res) {
-  console.log('RES DATA from SIGNUP.js: ', req.body)
+// FIND ALL EVENTS FOR EACH USER
+dbRouter.get('/events', function(req, res) {
+  db.EventMember.findAll({
+    where: {
+      userId: req.query.userId
+    }
+  }).then(function(events) {
+    if (events) {
+      res.json(events)
+    } else {
+      res.send('User is not part of any events.')
+    }
+  });
+});
+
+// CREATE EVENT WITH FRIENDS
+dbRouter.post('/events', function(req, res) {
+// if req.body.members is an array of userIds + creatorId
+var createMembers = function (array, eventId) {
+  var jsonArray = [];
+  array.forEach(function(memberId) {
+    jsonArray.push({eventId: eventId, userId: memberId});
+  })
+  return jsonArray;
+}
+
+  db.Event.create({
+    event_name: req.body.eventName,
+    event_creatorId: req.body.creatorId
+  }).then(function(event) {
+    if (event) {
+      db.EventMember.bulkCreate(createMembers(req.body.members, event.id))
+      .then(function() {
+        return db.EventMember.findAll({
+          where: {
+            eventId: event.id
+          }
+        });
+      }).then(function(members) {
+        if (members) {
+          res.json(members)
+        } else {
+          res.send('Event does not have members.')
+        }
+      })
+    } else {
+      res.send('No event');
+    }
+  })
+});
+
+// SIGNUP USER
+dbRouter.post('/signup', function(req, res) {
   // use findOne instead of findOrCreate because we're not using
   // 'where' to map to a unique index (creating duplicates)
   db.User.findOne({
@@ -49,7 +100,6 @@ dbRouter.post('/api/db/signup', function(req, res) {
       user_email: req.body.email
     }
   }).then(function(user) {
-    console.log('USER from SIGNUP: ', user);
     if (user) {
       res.send('You have already signed up. Please log in.')
     } else {
@@ -60,23 +110,20 @@ dbRouter.post('/api/db/signup', function(req, res) {
         user_streetaddress: req.body.streetaddress,
         user_postalcode: req.body.postalcode
       }).then(function(user) {
-        console.log('USER after create: ', user);
         res.json(user);
       })
     }
   })
 })
 
-
-dbRouter.post('/api/db/login', function(req, res) {
-  console.log('REQ BODY: ', req.body.email)
+// LOGIN USER
+dbRouter.post('/login', function(req, res) {
   db.User.findOne({
     where: {
       user_email: req.body.email,
       user_password: req.body.password
     }
   }).then(function(user) {
-    console.log('USER from LOGIN: ', user);
     if (user) {
       res.json(user)
     } else {
