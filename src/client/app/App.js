0f79b11d4e18d1637d85fa083f6b0a40931c66dd
Jennifer Sha <jennifersha@Jennifers-MacBook-Pro.local>
2017-02-05 03:57:04 -0800

    (develop) Make it so that user id is accessible in room page

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -5,7 +5,7 @@ import HeaderBar from './headerbar';
 import FriendList from './friend_list';
 import UserDetail from './User_detail';
 import SearchFriendBar from './Search_Friend_Bar';
-
+import axios from 'axios';
 
 // These data will be ajax from (sql)db 
 // we will have storage of friends
@@ -75,8 +75,15 @@ class App extends React.Component {
 	 // Generate new Page 
 	 // to newRoute
 	makeAPage() {
-		console.log('window.userid', window.userid);
-	 	console.log('make page')
+
+		axios.post('/api/db/check')
+    .then(function(response) {
+
+      if (response.data) {
+        browserHistory.push('/room');
+      } 
+
+	 	})
 	}
 	
 	// I pass down currentFriendList to child & also I passdown the changeFriendList 
@@ -120,7 +127,7 @@ class App extends React.Component {
   							<FriendList friends={this.state.currentUserFriendList}/>
   						</div>
   					</div>
-          <button onClick={this.makeAPage.bind(this)} className='btn btn-primary'><Link to={'room'}>Create new page</Link></button>
+          <button onClick={this.makeAPage.bind(this)} className='btn btn-primary'>Create new page</button>
   			</div>
       </div>
 		);
