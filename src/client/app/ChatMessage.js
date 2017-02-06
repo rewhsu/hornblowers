5e6d638da9864e95877b5eb580e585c68c91bbec
Andrew Hsu <andrewhsu42@gmail.com>
2017-02-06 14:38:40 -0800

    Implement username in messages

--- src/client/app/ChatMessage.js
+++ src/client/app/ChatMessage.js
@@ -8,53 +8,27 @@ class ChatMessage extends React.Component {
     super(props);
     this.state = {
       userId: this.props.message.userId,
-      username: null,
+      username: this.props.username,
       userVis: true,
       userData: {}
     }
-    this.toggleUsername = this.toggleUsername.bind(this);
-    this.getUsername = this.getUsername.bind(this);
+    this.toggleMessage = this.toggleMessage.bind(this);
   }
 
-  toggleUsername() {
-    console.log('username toggled');
-    this.setState({userVis: !this.state.userVis});
-  }
-
-  getUsername(e) {
-    console.log('msg',this.props.message)
-    var context = this;
-    axios
-      .get('/api/db/users', {
-        params: {
-          userId: this.props.message.userId
-        }
-      }).then(function(response) {
-        context.setState({
-          userData: response.data,
-          username: response.data.user_name
-        })
-      });
-  }
-
-  componentDidMount() {
-    this.getUsername();
+  toggleMessage() {
+    console.log('message toggled');
+    this.setState({msgVis: !this.state.msgVis});
   }
 
   render() {
     var showUser;
-    var chatGroupClass;
     if (!this.state.userVis) {
       showUser = {display: "none"};
-    //   chatGroupClass = "chat-group-single";
-    // } else {
-    //   chatGroupClass = "chat-group-double";
-    // }
     }
     return (
-      <div className="chat-group-outer" onClick={this.toggleUsername} >
+      <div className="chat-group-outer" onClick={this.toggleMessage} >
         <div className="chat-group">
-          <span className="chat-user-self" style={showUser}>{this.state.username}<br /></span>
+          <span className="chat-user-self" style={showUser}>{this.props.message.User.user_name}<br /></span>
           <span className="chat-message-other">{this.props.message.message_text}<br /></span>
         </div>
       </div>
